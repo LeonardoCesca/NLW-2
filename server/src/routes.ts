a5d1bd450208aae4916062083d952aacbcee8ce2
leonardocesca <leonardocescaf@gmail.com>
2020-08-04 15:47:20 -0300

    add: insert into class_schedule table

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,15 @@
 import express from 'express';
 import db from './database/connection';
 
+import convertHourToMinutes from './utils/convertHourToMinutes';
+
 const routes = express.Router();
+
+interface ScheduleItem {
+    week_day: number,
+    from: string,
+    to: string
+}
 
 routes.post('/classes', async (request, response) => {
     const {
@@ -30,6 +38,17 @@ routes.post('/classes', async (request, response) => {
     })
 
     const class_id = insertedClassesIds[0];
+
+    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+        return {
+            class_id,
+            week_day: scheduleItem.week_day,
+            from: convertHourToMinutes(scheduleItem.from),
+            to: convertHourToMinutes(scheduleItem.to),
+        };
+    })
+
+    await db('class_schedule').insert(classSchedule);
     
     return response.send();
 });
