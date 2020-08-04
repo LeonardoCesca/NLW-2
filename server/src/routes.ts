79c58166ee60982cef23fc28a13d7da1b6d0f1cc
leonardocesca <leonardocescaf@gmail.com>
2020-08-04 15:16:11 -0300

    add: routes file

---
+++ server/src/routes.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import db from './database/connection';
+
+const routes = express.Router();
+
+routes.post('/classes', async (request, response) => {
+    const {
+        name,
+        avatar,
+        whatsapp,
+        bio,
+        subject, 
+        cost,
+        schedule
+    } = request.body;
+
+    const insertedUserIds = await db('users').insert({
+        name,
+        avatar,
+        whatsapp,
+        bio
+    });
+
+    const user_id = insertedUserIds[0];
+
+    await db('classes').insert({
+        subject,
+        cost,
+        user_id,
+    })
+
+    return response.send();
+});
+
+export default routes;
