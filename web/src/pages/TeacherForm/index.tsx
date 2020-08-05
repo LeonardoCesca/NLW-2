c948a4fe5d3864aaf0401508516efd82db20e878
leonardocesca <leonardocescaf@gmail.com>
2020-08-05 08:21:50 -0300

    add: description on page header component

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -4,7 +4,10 @@ import PageHeader from '../../components/PageHeader';
 function TeacherForm() {
     return (
         <div id="page-teacher-form" className="container">
-            <PageHeader title="Estes são os proffys disponíveis." />
+            <PageHeader 
+                title="Estes são os proffys disponíveis."
+                description = "O primeiro passo é preencher o formulário de inscrição"                
+            />
         </div>
     )
 }
