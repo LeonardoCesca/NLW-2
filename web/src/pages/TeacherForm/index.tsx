8c4baad105919c6b6af24dc9e9898751a733c0f8
leonardocesca <leonardocescaf@gmail.com>
2020-08-05 19:56:59 -0300

    update: Input component

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+
+import './styles.css';
 
 function TeacherForm() {
     return (
@@ -8,6 +11,17 @@ function TeacherForm() {
                 title="Estes são os proffys disponíveis."
                 description = "O primeiro passo é preencher o formulário de inscrição"                
             />
+
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+
+                    <Input name="name" label="Nome Completo" />
+                    <Input name="avatar" label="Avatar" />
+                    <Input name="whatsapp" label="Whatsapp" />
+
+                </fieldset>
+            </main>
         </div>
     )
 }
